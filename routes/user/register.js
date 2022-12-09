3d53b525c54e8172f518508e7d10bf09360d9d47
daffa.rasyid <daffa.naufan@gmail.com>
2022-12-10 00:32:40 +0700

    update

--- routes/user/register.js
+++ routes/user/register.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs");
 
 const register = (router) => {
   router.post("/register", async (req, res) => {
-    const { name, email, password, phoneNumber } = req.body;
+    const { name, email, password, phoneNumber, role } = req.body;
 
     try {
       const checkEmail = await User.findOne({ email: email });
@@ -29,18 +29,36 @@ const register = (router) => {
 
       const token = jwt.sign({ email: email }, process.env.SECRET_KEY);
 
-      const user = await User.create({
-        name,
-        email,
-        password: hashPassword,
-        phoneNumber,
-        token,
-      });
-      return res.json({
-        success: true,
-        msg: "success create data",
-        data: user,
-      });
+      if (role == undefined) {
+        const user = await User.create({
+          name,
+          email,
+          password: hashPassword,
+          phoneNumber,
+          token,
+        });
+
+        return res.json({
+          success: true,
+          msg: "success create data",
+          data: user,
+        });
+      } else {
+        const user = await User.create({
+          name,
+          email,
+          password: hashPassword,
+          phoneNumber,
+          token,
+          role: 0,
+        });
+
+        return res.json({
+          success: true,
+          msg: "success create data",
+          data: user,
+        });
+      }
     } catch (e) {
       return res.json({ msg: e.message });
     }
