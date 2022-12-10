01124c8014ddb4cf66bb0c1b4dd64016a7587b00
daffa.rasyid <daffa.naufan@gmail.com>
2022-12-10 15:29:27 +0700

    update

--- routes/category/getCategory.js
+++ routes/category/getCategory.js
@@ -4,6 +4,7 @@ const getCategory = (router) => {
   router.post("/get-category", async (req, res) => {
     try {
       const category = await Category.find();
+
       if (category === []) {
         return res.json({
           success: true,
@@ -11,6 +12,20 @@ const getCategory = (router) => {
           data: [],
         });
       } else {
+        hasil = [];
+        for (let i = 0; i < category.length; i++) {
+          const categoryItem = category[i];
+
+          const hasilItem = {
+            _id: categoryItem._id,
+            name: categoryItem.name,
+            imageUrl: `http://103.226.139.23:3000/${categoryItem.imageUrl}`,
+            hotelId: categoryItem.hotelId,
+          };
+
+          hasil.push(hasilItem);
+        }
+
         return res.json({
           success: true,
           msg: "success getting data",
